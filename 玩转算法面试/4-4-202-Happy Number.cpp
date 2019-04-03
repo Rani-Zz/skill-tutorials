776149942ea9bb0b09808a2be7d33eb755d7deef
Rani_Zz <850150293@qq.com>
2019-04-03 10:15:14 +0800

    完整代码

---
+++ 玩转算法面试/4-4-202-Happy Number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int count(int n)
+    {
+        int tmp = 0;
+        while(n!=0)
+        {
+            int i = n%10;
+            tmp+=i*i;
+            n/=10;
+        }
+        
+        return tmp;
+    }
+public:
+    /**
+    *结果不为1是一直计算，把结果结果保存下来，当出现重复时return false
+    */
+    bool isHappy(int n) {
+        set<int> record;
+        while(n!=1)
+        {
+            if(record.find(n)!=record.end())
+                return false;
+            record.insert(n);
+            n = count(n);
+        }
+        return true;
+    }
+};
