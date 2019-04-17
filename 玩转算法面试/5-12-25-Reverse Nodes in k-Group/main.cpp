3ab1f123c72f3c92f2bb8dfcfc9004c6ba4429d5
Rani_Zz <850150293@qq.com>
2019-04-17 13:54:56 +0800

    Solution

--- 玩转算法面试/5-12-25-Reverse Nodes in k-Group/main.cpp
+++ 玩转算法面试/5-12-25-Reverse Nodes in k-Group/main.cpp
@@ -9,11 +9,15 @@
 class Solution {
 public:
     /**
-    *统计总的节点数
-    *翻转后减去k看剩下的结点还能否翻转
+    *pre指向可翻转的一段的前一位
+    *cur指向要翻转的前一个结点
+    *next指向要翻转的结点 头插的方式加到pre的后面
+    *tmp记录next的next
+    *之后要翻转的是tmp，所以next指向tmp
+    *翻转完一段后pre指向cur
     */
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head==NULL||k==1) 
+        if(head==NULL||head->next==NULL||k==1)
             return head;
         ListNode* dummy = new ListNode(-1);
         dummy->next = head;
@@ -22,25 +26,25 @@ public:
         int count = 0;
         while(p!=NULL)
         {
-            count++;
             p = p->next;
+            count++;
         }
         
         while(count>=k)
         {
-            ListNode* cur = pre->next;
-            ListNode* next = cur->next;
-            int t = k-1;
-            while(t--)
-            {
-                ListNode* tmp = next->next;
-                next->next = pre->next; //头插
-                pre->next = next;
-                cur->next = tmp;
-                next  = tmp;
-            }
-            pre = cur;
-            count-=k;
+          ListNode* cur = pre->next;
+          ListNode* next = cur->next;
+          int t = k-1;
+          while(t--)
+          {
+              ListNode* tmp = next->next;
+              next->next = pre->next;
+              pre->next = next;
+              cur->next = tmp;
+              next = tmp;
+          }
+          pre = cur;
+          count-=k;
         }
         
         ListNode* res = dummy->next;
