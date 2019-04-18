1e0ed7715145a8382536528856d3973e038164ad
Rani_Zz <850150293@qq.com>
2019-04-18 14:19:50 +0800

    Solution

--- 玩转算法面试/5-12-25-Reverse Nodes in k-Group/main.cpp
+++ 玩转算法面试/5-12-25-Reverse Nodes in k-Group/main.cpp
@@ -9,42 +9,48 @@
 class Solution {
 public:
     /**
-    *pre指向可翻转的一段的前一位
-    *cur指向要翻转的前一个结点
-    *next指向要翻转的结点 头插的方式加到pre的后面
-    *tmp记录next的next
-    *之后要翻转的是tmp，所以next指向tmp
-    *翻转完一段后pre指向cur
+    *统计结点个数
+    *判断剩余结点是否>=k
+    *翻转
+    *pre 指向要翻转的一段的前一个结点
+    *cur 指向要翻转的前一个结点
+    *next 指向要翻转的结点
+    *翻转 next头插法到pre tmp指向next->next cur指向tmp
+    *翻转后 pre next cur tmp
+    *next = tmp
+    *翻转玩完一段后 pre = cur
     */
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head==NULL||head->next==NULL||k==1)
+        if(head==NULL||k==1)
             return head;
-        ListNode* dummy = new ListNode(-1);
-        dummy->next = head;
-        ListNode* pre = dummy;
         ListNode* p = head;
         int count = 0;
         while(p!=NULL)
         {
-            p = p->next;
             count++;
+            p = p->next;
         }
+        
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* pre = dummy;
         
         while(count>=k)
         {
-          ListNode* cur = pre->next;
-          ListNode* next = cur->next;
-          int t = k-1;
-          while(t--)
-          {
-              ListNode* tmp = next->next;
-              next->next = pre->next;
-              pre->next = next;
-              cur->next = tmp;
-              next = tmp;
-          }
-          pre = cur;
-          count-=k;
+            ListNode* cur = pre->next;
+            ListNode* next = cur->next;
+            int t = k-1;
+            while(t--)
+            {
+                ListNode* tmp = next->next;
+                next->next = pre->next;
+                pre->next = next;
+                
+                cur->next = tmp;
+                next = tmp;
+            }
+            pre = cur;
+            count-=k;
         }
         
         ListNode* res = dummy->next;
