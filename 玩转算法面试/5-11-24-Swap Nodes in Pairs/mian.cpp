33305d710a66a2138027846a9ad818e6817de267
Rani_Zz <850150293@qq.com>
2019-04-20 12:19:40 +0800

    Solution

--- 玩转算法面试/5-11-24-Swap Nodes in Pairs/mian.cpp
+++ 玩转算法面试/5-11-24-Swap Nodes in Pairs/mian.cpp
@@ -8,28 +8,34 @@
  */
 class Solution {
 public:
+    /**
+    *建立dummy
+    *pre 指向翻转前
+    *node1 pre->next node2 pre->next->next
+    *
+    */
     ListNode* swapPairs(ListNode* head) {
         if(head==NULL||head->next==NULL)
             return head;
         ListNode* dummy = new ListNode(-1);
         dummy->next = head;
+        
         ListNode* pre = dummy;
         while(pre->next!=NULL&&pre->next->next!=NULL)
         {
             ListNode* node1 = pre->next;
             ListNode* node2 = node1->next;
-            ListNode* s = node2->next;
             
-            node2->next = node1;
+            ListNode* tmp = node2->next;
+            node2->next = pre->next;
             pre->next = node2;
-            node1->next = s;
+            node1->next = tmp;
             
             pre = node1;
         }
         
         ListNode* res = dummy->next;
         delete dummy;
-        
         return res;
     }
 };
