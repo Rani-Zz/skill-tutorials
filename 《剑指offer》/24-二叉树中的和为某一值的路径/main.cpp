60d393b52c4d053304514354e9c57ca4b31ff3b0
Rani_Zz <850150293@qq.com>
2019-05-08 11:17:38 +0800

    Solution

--- 《剑指offer》/24-二叉树中的和为某一值的路径/main.cpp
+++ 《剑指offer》/24-二叉树中的和为某一值的路径/main.cpp
@@ -1,30 +1,19 @@
-/*
-struct TreeNode {
-	int val;
-	struct TreeNode *left;
-	struct TreeNode *right;
-	TreeNode(int x) :
-			val(x), left(NULL), right(NULL) {
-	}
-};*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
-private:
-    vector<vector<int>> res;
-    vector<int> tmp;
 public:
-    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
-        if(root==NULL)
-            return res;
-        tmp.push_back(root->val);
-        expectNumber-=root->val;
-        if(expectNumber==0&&root->left==NULL&&root->right==NULL)
-            res.push_back(tmp);
-        if(root->left)
-            FindPath(root->left,expectNumber);
-        if(root->right)
-            FindPath(root->right,expectNumber);
-        if(!tmp.empty())
-            tmp.pop_back();
-        return res;
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(root==NULL) return false;
+        sum-=root->val;
+        if(root->left==NULL&&root->right==NULL&&sum==0)
+            return true;
+        return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);
     }
 };
