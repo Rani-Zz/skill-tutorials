8f1aa9c6b6f8adf65b091552bd5b13a3fc131aef
Rani_Zz <850150293@qq.com>
2019-05-11 17:37:33 +0800

    Solution

--- 玩转算法面试/7-2-111-Minimum Depth of Binary Tree/main.cpp
+++ 玩转算法面试/7-2-111-Minimum Depth of Binary Tree/main.cpp
@@ -20,8 +20,10 @@ private:
 public:
     /**
     *后序遍历 返回左右子树深度较小的
-    *遍历到根节点停止
+    *遍历到根节点停止 如果不停止就是左右都是INT_MAX,结点深度在参数后加的话会越界
+	*所以深度要在遍历结点前就带上，到根结点即左右都为NULL时直接返回 
     *但是空结点深度不能返回0，所以为NULL时返回INT_MAX
+    *
     */
     int minDepth(TreeNode* root) {
         if(root==NULL) return 0;
